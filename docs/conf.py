4cc5c687929e80cf535b27ef11c807a287034792
ouyangjiahong <ouyangjiahong@github.com>
2026-04-24 08:46:13 +0800

    docs: add Sphinx configuration and index
    - Create conf.py with autodoc, napoleon, mathjax, viewcode, intersphinx,
      mermaid, and copybutton extensions
    - Create index.rst with toctree for all documentation sections
    - Create standard Sphinx Makefile

---
+++ docs/conf.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""Sphinx configuration for e2m2e documentation."""
+
+import os
+import sys
+
+# 添加项目根目录到 path
+sys.path.insert(0, os.path.abspath(".."))
+
+# -- General configuration ------------------------------------------------
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinxcontrib.mermaid",
+    "sphinx_copybutton",
+]
+
+# 中文支持
+language = "zh_CN"
+locale_dirs = ["locale/"]
+
+# 源文件后缀
+source_suffix = ".rst"
+master_doc = "index"
+
+# 项目信息
+project = "e2m2e"
+copyright = "2026, ouyangjiahong"
+author = "ouyangjiahong"
+
+# 版本信息
+from e2m2e import __version__
+
+release = __version__
+
+# -- Options for HTML output ----------------------------------------------
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
+# -- Extension configuration ----------------------------------------------
+autodoc_member_order = "bysource"
+autodoc_typehints = "description"
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+
+# Intersphinx mapping
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
+}
---
+++ docs/index.rst
@@ -0,0 +1,45 @@
+.. e2m2e documentation master file
+
+e2m2e: 地月转移轨道设计库
+===================================
+
+e2m2e (Earth to Moon, Moon to Earth) 是一个基于圆型限制性三体问题 (CR3BP) 的
+地月转移轨道设计 Python 库。
+
+.. toctree::
+   :maxdepth: 2
+   :caption: 目录
+
+   getting-started/installation
+   getting-started/quickstart
+   getting-started/visualization
+
+   core/system
+   core/dynamics
+   core/orbit
+   core/coordinate
+
+   algorithms/differential-correction
+   algorithms/continuation
+   algorithms/halo
+   algorithms/stability
+   algorithms/multiple-shooting
+
+   transfer/overview
+   transfer/search
+   transfer/optimization
+
+   api/e2m2e
+   api/e2m2e.core
+   api/e2m2e.algorithms
+   api/e2m2e.transfer
+
+   reference/glossary
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
